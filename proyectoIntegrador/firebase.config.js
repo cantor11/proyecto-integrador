4cfbd6fa9fbff8fa9b68e2f772b0ffcdc7234c11
JuanC513U <juan.esteban.clavijo@correounivalle.edu.co>
2024-09-01 12:33:50 -0500

    Se agregan variables de entorno a la lógica con el servicio de FireBase / FireStore

--- proyectoIntegrador/firebase.config.js
+++ proyectoIntegrador/firebase.config.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDXt_rwO5RYQhQGlgEu1h-FfXTaY3Svn-k",
-    authDomain: "proyecto-integrador-11.firebaseapp.com",
-    projectId: "proyecto-integrador-11",
-    storageBucket: "proyecto-integrador-11.appspot.com",
-    messagingSenderId: "236921769662",
-    appId: "1:236921769662:web:e881c940fc38056b0298ef"
+    apiKey: import.meta.env.VITE_REACT_APP_API_KEY,
+    authDomain: import.meta.env.VITE_REACT_APP_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_REACT_APP_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_REACT_APP_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_REACT_APP_APP_ID
 };
 
 // Initialize Firebase
